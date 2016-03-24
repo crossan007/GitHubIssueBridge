43bdcd4aedcd967b4a0689724eec63147de2289b
crossan007 <crossan007@gmail.com>
2016-03-23 23:20:12 -0400

    proof of concept

--- GitHubIssueBridge/index.php
+++ GitHubIssueBridge/index.php
@@ -1,16 +1,35 @@
 <?php
 
+function FilterInput($value)
+{
+  if (isset($value))
+  {
+    return $value;
+  }
+  else
+  {
+    die("Input requred");
+  }
+}
+
 require "config.php";
-if (isset($_POST['issueTitle']))
+
+$serviceURL = "https://api.github.com/repos/" . $GitHubRepoOwner . "/" . $GitHubRepoName . "/issues";
+$postdata = new stdClass();
+$postdata->title = FilterInput($_GET['issueTitle']);
+$postdata->body = FilterInput($_GET['issueDescription']);
+$postdata->labels = array("Web Report");
+
+$curlService = curl_init($serviceURL);
+
+curl_setopt($curlService, CURLOPT_POST, true);
+curl_setopt($curlService, CURLOPT_USERPWD, $GitHubUserName . ":" . $GitHubPassword);
+curl_setopt($curlService, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1');
+curl_setopt($curlService, CURLOPT_POSTFIELDS, json_encode($postdata));
+
+$result = curl_exec($curlService);
+if ($result === FALSE)
 {
-  echo $_POST['issueTitle'];
-}
-if (isset($_POST['issueDescription']))
-{
-  echo $_POST['issueDescription'];
-}
-if (isset($_POST['issueAttachments']))
-{
-  echo "attachments";
+  die(curl_error($curlService));
 }
 ?>
