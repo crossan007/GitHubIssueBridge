c9bddda0d6b639e93ff6470ea783b0bf70dd639d
crossan007 <crossan007@gmail.com>
2016-03-24 14:25:37 -0400

    Tweaks

--- GitHubIssueBridge/index.php
+++ GitHubIssueBridge/index.php
@@ -11,13 +11,16 @@ function FilterInput($value)
     die("Input requred");
   }
 }
-
+//echo  '{"number": 21, "url": "https://github.com/crossan007/GitHubIssueBridge/issues/21"}';
+//exit;
 require "config.php";
 
 $serviceURL = "https://api.github.com/repos/" . $GitHubRepoOwner . "/" . $GitHubRepoName . "/issues";
+
+$receivedData = json_decode(file_get_contents('php://input'));
 $postdata = new stdClass();
-$postdata->title = FilterInput($_GET['issueTitle']);
-$postdata->body = FilterInput($_GET['issueDescription']);
+$postdata->title = FilterInput($receivedData->issueTitle);
+$postdata->body = FilterInput($receivedData->issueDescription);
 $postdata->labels = array("Web Report");
 
 $curlService = curl_init($serviceURL);
@@ -26,10 +29,16 @@ curl_setopt($curlService, CURLOPT_POST, true);
 curl_setopt($curlService, CURLOPT_USERPWD, $GitHubUserName . ":" . $GitHubPassword);
 curl_setopt($curlService, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1');
 curl_setopt($curlService, CURLOPT_POSTFIELDS, json_encode($postdata));
+curl_setopt($curlService,CURLOPT_RETURNTRANSFER,true);
 
 $result = curl_exec($curlService);
 if ($result === FALSE)
 {
   die(curl_error($curlService));
 }
+$issue = json_decode($result);
+$returnIssue = new stdClass();
+$returnIssue->number = $issue->number;
+$returnIssue->url = $issue->html_url;
+echo json_encode($returnIssue);
 ?>
